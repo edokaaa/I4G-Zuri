07aefa1ef68d0f31de8676781b0a83feb8260610
peter4ease <40390930+peter4ease@users.noreply.github.com>
2022-06-25 10:53:14 +0100

    completed week6 basic tasks

--- week6/calculator.js
+++ week6/calculator.js
@@ -1,1 +1,26 @@
-/* basic calculator using js */
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function divide(x, y) {
+    return x/y
+}
+
+function multiply(x,y) {
+    return x*y
+}
+
+function modulus(x,y) {
+    return x%y
+}
+
+
+console.log(add(4,5));
+console.log(subtract(10,5));
+console.log(divide(20, 9));
+console.log(multiply(9,4));
+console.log(modulus(8,2));
---
+++ week6/me.js
@@ -0,0 +1,1 @@
+console.log('My Name is Peter Edoka Augustine, I am 180cm tall, I am from Nigeria.')
